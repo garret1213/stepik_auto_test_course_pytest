1b919ab24f30d1f6365d65e54059a4df02816762
garret1213 <nigmatull12@yandex.ru>
2022-01-07 17:53:31 +0300

    закомментировал sleep

--- test/test_items.py
+++ test/test_items.py
@@ -6,7 +6,7 @@ import pytest
 def test_add_to_cart_button(browser, user_language):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    sleep(1)
+    #sleep(30)
 
     button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert button, 'кнопка не найдена'
